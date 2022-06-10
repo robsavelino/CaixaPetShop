397c1d6bfa53ec37e8dc00324e69ef455952429d
Roberto Avelino <roberto.avelino97@hotmail.com>
2022-06-10 20:49:42 -0300

    Add project files.

---
+++ Caixa.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaixaPetShop
+{
+    public class Caixa
+    {
+        public DateTime DataCaixa { get; private set; }
+
+        public decimal Saldo { get; private set; }
+
+        public void AbrirCaixa(decimal saldoInicial = 0)
+        {
+            DataCaixa = DateTime.Now;
+            Saldo = saldoInicial;
+        }
+        public void Deposita(decimal valor)
+        {
+            if (valor < 0)
+                return;
+
+            Saldo += valor;
+        }
+
+        public void Saca(decimal valor)
+        {
+            if (Saldo >= valor)
+            {
+                Saldo -= valor;
+            }
+            else
+            {
+                Console.WriteLine("Saldo insuficente");
+                Console.WriteLine($"O saldo atual é de R$ {Saldo}");
+            }
+        }
+
+        public decimal CalcularTroco(decimal venda, decimal valor)
+        {
+            if (venda > valor)
+            {
+                Console.WriteLine("Saldo insuficiente");
+                return -1;
+            }
+            return valor - venda;
+        }
+
+        public decimal DevolverValor(decimal venda, decimal valor)
+        {
+
+
+            if (CalcularTroco(venda, valor) == -1)
+            {
+                decimal resto = venda - valor;
+
+                Console.WriteLine($"Faltam R${resto} para o valor total da compra");
+                Console.WriteLine("Digite o valor restante que deseja pagar");
+
+                valor = Convert.ToDecimal(Console.ReadLine());
+
+                DevolverValor(resto, valor);
+            }
+
+            Deposita(valor);
+            Saca(CalcularTroco(venda, valor));
+
+            return CalcularTroco(venda, valor);
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using CaixaPetShop;
+using System.Threading.Tasks;
+
+Caixa caixaDoDia = new Caixa();
+caixaDoDia.AbrirCaixa();
+
+Console.WriteLine($"Caixa Aberto em {caixaDoDia.DataCaixa}");
+
+decimal valorTotal;
+decimal valorPago;
+string aux;
+
+do
+{
+    Console.Clear();
+    Console.WriteLine("Digite o valor da consulta: ");
+    aux = Console.ReadLine();
+
+    if(!Decimal.TryParse(aux, out valorTotal))
+    {
+        Console.WriteLine("Digite um valor valido");
+        Thread.Sleep(1500);
+    }  
+} while (!Decimal.TryParse(aux, out valorTotal));
+
+
+do
+{
+    Console.Clear();
+    Console.WriteLine("Digite o valor do dinheiro que deseja pagar a consulta: ");
+    aux = Console.ReadLine();
+
+    if (!Decimal.TryParse(aux, out valorTotal))
+    {
+        Console.WriteLine("Digite um valor valido");
+        Thread.Sleep(1500);
+    }
+} while (!Decimal.TryParse(aux, out valorPago));
+
+
+Console.WriteLine($"O troco sera: ");
